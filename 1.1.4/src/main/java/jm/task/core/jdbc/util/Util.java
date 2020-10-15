6bc01e894ed76ec364d52db247e2e7b62ac4b726
esazikov <understander@mail.ru>
2020-10-15 15:27:14 +0500

    Первое приближенеие к Хибернэйт

---
+++ 1.1.4/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,46 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util implements AutoCloseable{
+
+    private static Connection connection = null;
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?useUnicode=true&serverTimezone=UTC&useSSL=false", "admin", "12345");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null){
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?useUnicode=true&serverTimezone=UTC&useSSL=false", "admin", "12345");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
