82051777f5b5c21ce80d9263e5e5f48bd77249db
esazikov <understander@mail.ru>
2020-10-15 15:37:20 +0500

    Отдельный commit и AutoCommit в finally

--- 1.1.3/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ 1.1.3/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -40,19 +40,34 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age){
         try {
             connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, lastname,age) VALUES (?, ?, ?)");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
-            connection.setAutoCommit(true);
+            connection.commit();
             System.out.println("User с именем - " + name + " добавлен в базу данных");
             preparedStatement.close();
         } catch (Exception e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
